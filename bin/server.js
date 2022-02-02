cc3e64c508a9bcb6f5abda4515f92649396646d0
jfcaballerop <jfcaballerop@gmail.com>
2022-02-02 19:14:43 +0100

    add mongo close connection on SIGINT

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const app = require('../app')
+const { mongoose } = require('../src/models')
 
 // set port, listen for requests
 const PORT = (process.env.NODE_ENV === 'test') ? process.env.NODE_TEST_LOCAL_PORT : process.env.NODE_DOCKER_PORT
@@ -17,3 +18,11 @@ process.on('unhandledRejection', err => {
   console.warn('OOOPS! Unhandled Promise rejection', err.message)
   console.log(err)
 })
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', () => {
+	mongoose.connection.close(() => {
+	  console.log('Mongoose disconnected on app termination');
+	  process.exit(0);
+	});
+  });
