caa52fd9405287de98400700410a217ec60a32e2
Jose Fernando Caballero <josefernando.caballero@logesta.com>
2022-02-07 12:29:05 +0100

    Add Validators Login

--- src/helpers/validators/userValidator.js
+++ src/helpers/validators/userValidator.js
@@ -1,3 +1,6 @@
-const { check } = require('express-validator/check')
+const { check } = require('express-validator')
 
-exports.loginUserValidate = [check('username').isLength({ min: 4 })]
+exports.loginUserValidate = [
+  check('username').exists().withMessage('Username can not be empty!').bail().isLength({ min: 4 }).withMessage('min length is 5 char'),
+  check('password').exists().withMessage('Password can not be empty!').bail().isLength({ min: 5 }).withMessage('password format is incorrect')
+]
